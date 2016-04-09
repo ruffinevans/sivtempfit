d395175232474a4be9cdbd06eb3d069989112f69
Ruffin Evans <ruffin@ruffinevans.com>
2016-04-09 10:23:31 -0400

    Commit with minimal package structure This is Part A of assignment L-10

---
+++ SiVTempFit/__init__.py
@@ -0,0 +1,13 @@
+# This file is automatically run when the package is imported
+# So, a few key definitions can be placed here.
+# It should import things from other .py files in this directory, e.g.
+# from .text import test_asdf()
+# See https://python-packaging.readthedocs.org/en/latest/minimal.html
+
+import numpy as np
+import seaborn as sns
+import emcee
+
+
+def test_asdf():
+    return 1
--- section_test.py
+++
@@ -1,2 +0,0 @@
-def test_asdf():
-    return 1
