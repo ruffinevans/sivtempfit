2cc4bd3dd5fb300c7d79c121f78a89a8ea4d43b1
Ruffin Evans <ruffin@ruffinevans.com>
2016-04-09 10:39:24 -0400

    Added framework for unit tests Unit tests *will not function* right now since there is nothing to test

---
+++ SiVTempFit/tests/__init__.py
@@ -0,0 +1,7 @@
+# This file is automatically run when the package is imported
+# So, a few key definitions can be placed here.
+# It should import things from other .py files in this directory, e.g.
+# from .text import test_asdf()
+# See https://python-packaging.readthedocs.org/en/latest/minimal.html
+
+import example_data_test
---
+++ SiVTempFit/tests/example_data_test.py
@@ -0,0 +1,12 @@
+# This set of tests will run on the example data to make sure it is in a
+# reasonable format
+
+from unittest import TestCase
+
+
+class TestSpectrum(TestCase):
+    def test_spectrum_format(self):
+        # Need to update this once we have example data.
+        # For now, will throw an error.
+        s = sivtempfit.examplespectrum()
+        self.assertTrue(isinstance(s, basestring))
