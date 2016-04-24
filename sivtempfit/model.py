2098620025904dd8857f4a72a59ae40f4695d10e
Ruffin Evans <ruffin@ruffinevans.com>
2016-04-24 13:48:17 -0400

    Updated model with naive but almost certainly correct likelihood Please see issue #19 for discussion

--- sivtempfit/model.py
+++ sivtempfit/model.py
@@ -1,8 +1,5 @@
-from . import dataprocessing as dp
-import json
-import pandas as pd
 import numpy as np
-import scipy as sp
+import scipy.stats as stats
 
 
 def lorentz(x, center, width):
@@ -10,10 +7,11 @@ def lorentz(x, center, width):
     A lorentzian function with peak position 'center' and FWHM 'width'.
     """
 
-    return (1/np.pi)*(width/2)/((x-center)**2+(width/2)**2)
+    return (1 / np.pi) * (width / 2) / ((x - center)**2 + (width / 2)**2)
 
-def two_peak_model(x, amp1, amp2, center_offset, center2, 
-    width1, width2, background):
+
+def two_peak_model(x, amp1, amp2, center_offset, center2,
+                   width1, width2, background):
     """
     Model for the data: two lorentzian peaks with variable amplitudes, widths,
     and center positions.
@@ -27,16 +25,20 @@ def two_peak_model(x, amp1, amp2, center_offset, center2,
     For a full explanation, see the model-development.ipynb notebook.
     """
 
-    return (background + 
-        amp1*lorentz(x, center_offset + center2, width1) + 
-        amp2*lorentz(x, center2, width2))
+    return (background +
+            amp1 * lorentz(x, center_offset + center2, width1) +
+            amp2 * lorentz(x, center2, width2))
 
-def two_peak_log_likelihood(x, y, amp1, amp2, T, m, C0, center2, width1, 
-    width2, light_background, ccd_background, ccd_stdev, conv_range = 6):
+
+def two_peak_log_likelihood(x, y, amp1, amp2, T, m, C0, center2,
+                            width1, width2, light_background,
+                            ccd_background, ccd_stdev,
+                            conv_range = -1, debug = False,
+                            test_norm = True):
     """
     Returns the log-likelihood calculated for the two-peak + CCD noise model.
     See also: two_peak_model
-    
+
     Parameters:
     -----------
     x : wavelength or x-axis value
@@ -53,49 +55,89 @@ def two_peak_log_likelihood(x, y, amp1, amp2, T, m, C0, center2, width1,
                        contributing shot noise
     ccd_backgrond : The contribution to the background from CCD readout,
                     contributing gaussian noise
-    ccd_stdev : The standard deviation on the gaussian CCD noise above
-    conv_range : How far out to go in convolution. 
-                 Default is 6 sigma on the gaussian contribution.
+    ccd_stdev : The standard deviation on the gaussian CCD noise
+
+    Optional Arguments:
+    -------------------
+    conv_range : How far out to go in convolution. Default is -1, which
+                 goes out to max([max(y), max(model_prediction)]) plus
+                 3 * sqrt of this max.
+    debug : if True, returns the convolution list, the convolution
+            matrix, the poisson matrix, the gaussian matrix, the
+            likelihood list, and the computed max_y. False by default,
+            in which case only the log-likelihood is returned.
+    test_norm: if True, tests to make sure that the gaussian and poisson
+               contributions are normalized. This is the default case.
+               If False, the function will run a little faster but will
+               not check this condition.
     """
 
     # First, get the contribution from the light signal.
-    y_two_peak_model = two_peak_model(x, amp1, amp2, C0 + T*m, center2,
-        width1, width2, light_background)
+    y_two_peak_model = two_peak_model(x, amp1, amp2, C0 + T * m,
+                                      center2, width1, width2,
+                                      light_background)
+
     # Construct the bounds of convolution
-    # Adding +0.01 makes it so that if user passes an integer 
-    # e.g. 5, goes to +/- 5 and not -5 to 4.
-    conv_list = np.arange(-conv_range*ccd_stdev, 
-        (conv_range+0.01)*ccd_stdev)[:,np.newaxis]
+    # If the conv_range is -1, go out to the max of the
+    # max(y, model prediction) + 5x the square root of the max.
+    max_y = 0
+    if conv_range == -1:
+        max_y = np.max([np.max(y_two_peak_model), np.max(y)])
+        conv_max = max_y + 3 * np.sqrt(max_y)
+    elif conv_range > 0:
+        conv_max = conv_range
+    else:
+        raise ValueError('Range for convolution must be positive')
+    conv_list = np.arange(0, conv_max)[:, np.newaxis]
     # Construct the convolution matrix by broadcasting
-    conv_mat = y - ccd_background + conv_list
-    # Make negative elements nan, because we can't take negative values
-    # in the poisson distribution.
-    conv_mat[conv_mat<0] = np.nan
+    conv_mat = (conv_list - y) + y
+
     # Construct poisson term
-    poisson_term = (y_two_peak_model**(conv_mat)/
-                    sp.misc.factorial(conv_mat)*
-                    np.exp(-y_two_peak_model))
+    poisson_term = stats.poisson.pmf(conv_mat, y_two_peak_model)
     # Construct gaussian term
-    gaussian_term = ((1/(np.sqrt(2*np.pi*ccd_stdev**2)))*
-                     np.exp(-1*(y - conv_mat - ccd_background)**2 / 
-                            (2*ccd_stdev**2)))
-    # Perform sum to get likelihoods
-    # Here we do nansum which treats nans as zero.
-    likelihood_list = np.nansum(poisson_term * gaussian_term, 0)
+    gaussian_term = ((1 / (np.sqrt(2.0 * np.pi * ccd_stdev**2))) *
+                     np.exp(-1.0 * (y - conv_mat - ccd_background)**2 /
+                            (2.0 * ccd_stdev**2)))
+    # Perform sum over zeroth axis to get likelihoods
+    likelihood_list = np.sum(poisson_term * gaussian_term, 0)
+
+    # Test to make sure poisson and gaussian terms are normalized.
+    # In other words, make sure the sum in the convolution is going
+    # out far enough.
+    # I think it is OK to test only one of them, because if we are
+    # going completely over the support of at least one of them then
+    # it means that we are not missing out on any probability density
+    # in the convolution.
+    # My motivation for doing `or` instead of `and` is that the gaussian
+    # term seems to suffer from some discretization errors. If the
+    # standard deviation is small, then points can land at non-integer
+    # y values, which can generate a low likelihood just for the gaussian
+    # term.
+    if test_norm:
+        gauss_norm = all(np.sum(gaussian_term.T, axis = 1) > 0.99)
+        poiss_norm = all(np.sum(poisson_term.T, axis = 1) > 0.99)
+        if not(gauss_norm or poiss_norm):
+            raise ValueError('The terms in the convolution are' +
+                             ' not normalized. Try increasing' +
+                             ' the conv_range variable!')
+    if debug:
+        return [conv_list, conv_mat, poisson_term,
+                gaussian_term, likelihood_list, conv_max]
     # Return sum of log_likelihoods
-    #return np.array([poisson_term, gaussian_term])
     return np.sum(np.log(likelihood_list))
 
-def two_peak_log_likelihood_Spectrum(spectrum, amp1, amp2, T, m, C0, center2, width1, 
-    width2, light_background, ccd_background, ccd_stdev, conv_range = 6):
+
+def two_peak_log_likelihood_Spectrum(spectrum, amp1, amp2, T, m, C0, center2,
+        width1, width2, light_background, ccd_background, ccd_stdev,
+        conv_range = -1, debug = False, test_norm = True):
     """
     Returns the log-likelihood calculated for the two-peak + CCD noise model.
     See also: two_peak_model
-    
+
     Parameters:
     -----------
     spectrum : data passed as an instance of Spectrum class
-    amp1 : amplitude of the broad SiV peak in the spectrum
+    amp1: amplitude of the broad SiV peak in the spectrum
     amp2 : amplitude of the narrow calibration peak in the spectrum
     T : The temperature of the sample
     m : The linear scaling of the SiV peak position with temperature
@@ -107,12 +149,24 @@ def two_peak_log_likelihood_Spectrum(spectrum, amp1, amp2, T, m, C0, center2, wi
                        contributing shot noise
     ccd_backgrond : The contribution to the background from CCD readout,
                     contributing gaussian noise
-    ccd_stdev : The standard deviation on the gaussian CCD noise above
-    conv_range : How far out to go in convolution. 
-                 Default is 6 sigma on the gaussian contribution.
-    """
+    ccd_stdev : The standard deviation on the gaussian CCD noise
 
+    Optional Arguments:
+    -------------------
+    conv_range : How far out to go in convolution. Default is -1, which
+                 goes out to max([max(y), max(model_prediction)]) plus
+                 3 * sqrt of this max.
+    debug : if True, returns the convolution list, the convolution
+            matrix, the poisson matrix, the gaussian matrix, the
+            likelihood list, and the computed max_y. False by default,
+            in which case only the log-likelihood is returned.
+    test_norm: if True, tests to make sure that the gaussian and poisson
+               contributions are normalized. This is the default case.
+               If False, the function will run a little faster but will
+               not check this condition.
+    """
     x = spectrum.data.values.T[1]
     y = spectrum.data.values.T[0]
     return two_peak_log_likelihood(x, y, amp1, amp2, T, m, C0, center2, width1, 
-    width2, light_background, ccd_background, ccd_stdev, conv_range)
+             width2, light_background, ccd_background, ccd_stdev, conv_range
+             , debug, test_norm)
--- sivtempfit/tests/likelihood_test.py
+++ sivtempfit/tests/likelihood_test.py
@@ -17,43 +17,43 @@ class TestLikelihood(TestCase):
         # that is consistent with the simulated data.
         # This is a fairly demanding test. It also tests the data loading.
 
-        test_amp_list = np.arange(400, 600, 0.5)
+        test_amp_list = np.arange(400, 600, 2)
         test_ll = [two_peak_log_likelihood_Spectrum(self.simulated_spectrum,
                    x, 10, 0, 0, -30, 70, 20, 0.1, 1, 1, 0.5) for x in test_amp_list]
         amp = test_amp_list[test_ll.index(max(test_ll))]
         self.assertTrue(amp < 505 and amp > 495)
 
     def test_MLE_amplitude2_inference(self):
-        test_amp_list = np.arange(5, 15, 0.25)
+        test_amp_list = np.arange(5, 15, 0.5)
         test_ll = [two_peak_log_likelihood_Spectrum(self.simulated_spectrum,
                    500, x, 0, 0, -30, 70, 20, 0.1, 1, 1, 0.5) for x in test_amp_list]
         amp = test_amp_list[test_ll.index(max(test_ll))]
         self.assertTrue(amp < 11 and amp > 9)
 
     def test_MLE_center1_inference(self):
-        test_amp_list = np.arange(-50, 80, 0.25)
+        test_amp_list = np.arange(-50, 80, 1)
         test_ll = [two_peak_log_likelihood_Spectrum(self.simulated_spectrum,
                    500, 10, 0, 0, x, 70, 20, 0.1, 1, 1, 0.5) for x in test_amp_list]
         amp = test_amp_list[test_ll.index(max(test_ll))]
         self.assertTrue(amp < -28 and amp > -32)
 
     def test_MLE_center2_inference(self):
-        test_amp_list = np.arange(50, 100, 0.05)
+        test_amp_list = np.arange(50, 100, 1)
         test_ll = [two_peak_log_likelihood_Spectrum(self.simulated_spectrum,
                    500, 10, 0, 0, -30, x, 20, 0.1, 1, 1, 0.5) for x in test_amp_list]
         amp = test_amp_list[test_ll.index(max(test_ll))]
         self.assertTrue(amp > 68 and amp < 72)
 
     def test_MLE_width1_inference(self):
-        test_amp_list = np.arange(10, 30, 0.1)
+        test_amp_list = np.arange(10, 30, 0.5)
         test_ll = [two_peak_log_likelihood_Spectrum(self.simulated_spectrum,
                    500, 10, 0, 0, -30, 70, x, 0.1, 1, 1, 0.5) for x in test_amp_list]
         amp = test_amp_list[test_ll.index(max(test_ll))]
         self.assertTrue(amp > 19 and amp < 21)
 
     def test_MLE_width2_inference(self):
-        test_amp_list = np.arange(0.05, 0.25, 0.005)
+        test_amp_list = np.arange(0.05, 0.25, 0.01)
         test_ll = [two_peak_log_likelihood_Spectrum(self.simulated_spectrum,
                    500, 10, 0, 0, -30, 70, 20, x, 1, 1, 0.5) for x in test_amp_list]
         amp = test_amp_list[test_ll.index(max(test_ll))]
-        self.assertTrue(amp > 0.09 and amp < 0.11)
+        self.assertTrue(amp > 0.08 and amp < 0.12)
